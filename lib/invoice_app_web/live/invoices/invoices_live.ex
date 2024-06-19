7c9f0c72a1b4f24e2ab1029c1632dc0320e77ccb
ngumokenneth <ngumokenneth97@gmail.com>
2024-06-19 22:38:44 +0300

    New invoice commit

--- lib/invoice_app_web/live/invoices/invoices_live.ex
+++ lib/invoice_app_web/live/invoices/invoices_live.ex
@@ -7,18 +7,52 @@ defmodule InvoiceAppWeb.Invoices.InvoicesLive do
 
   def render(assigns) do
     ~H"""
-    <div class="bg-[rgb(55,59,83)] flex justify-between h-44">
-      <div class="bg-[#7C5DFA] rounded-r-[40px] relative  h-full overflow-hidden">
+    <%!-- mobile top-bar --%>
+    <div class="bg-[rgb(55,59,83)] flex justify-between h-16 lg:hidden">
+      <div class="bg-[#7C5DFA]  relative  h-full overflow-hidden rounded-r-2xl w-16">
         <div class="absolute z-10 flex items-center justify-center w-full h-full">
-          <img src="/images/images/logo_white.svg" alt="" class="w-16 h-16" />
+          <img src="/images/images/logo_white.svg" alt="" class="h-7 w-7" />
         </div>
-        <div class="bg-[#9277FF] w-44 rounded-tl-[40px]  mt-20 px-2 py-28 relative bottom-0"></div>
+        <div class="absolute bottom-0 w-full h-8 px-2 bg-[#9277FF] rounded-tl-2xl"></div>
       </div>
-      <div class="flex items-center h-full pr-8 overflow-hidden">
-        <img src="/images/images/cresent.svg" alt="" class="w-16 h-16" />
-        <div class="w-1 h-48 bg-[#494E6E] mx-10"></div>
+      <div class="flex items-center h-full mx-4 overflow-hidden ">
+        <img src="/images/images/cresent.svg" alt="" class="h-7 w-7" />
+        <div class="border h-48 bg-[#494E6E] mx-5"></div>
+        <div><img src="/images/images/user_icon.svg" alt="" class="h-7 w-7" /></div>
+      </div>
+    </div>
 
-        <div><img src="/images/images/user_icon.svg" alt="" class="w-20 h-20" /></div>
+    <%!-- desktop top-bar --%>
+    <div class="hidden lg:block">
+      <div class="bg-[rgb(55,59,83)] flex justify-between w-20 flex-col h-screen rounded-r-2xl">
+        <div class="bg-[#7C5DFA] rounded-r-2xl h-20 flex flex-col relative items-center overflow-hidden">
+          <div class="absolute z-10 flex items-center justify-center h-full">
+            <img src="/images/images/logo_white.svg" alt="" class="h-7 w-7" />
+          </div>
+          <div class="absolute bottom-0 w-full h-10 bg-[#9277FF] rounded-tl-2xl"></div>
+        </div>
+        <div class="flex flex-col items-center my-4">
+          <img src="/images/images/cresent.svg" alt="" class="h-7 w-7" />
+          <div class="border w-full bg-[#494E6E] my-4"></div>
+          <div><img src="/images/images/user_icon.svg" alt="" class="w-10 h-10" /></div>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex items-center justify-between px-2 py-6">
+      <div>
+        <p class="text-xl font-extrabold">Invoices</p>
+        <p class="text-xs">No invoices</p>
+      </div>
+      <div class="flex font-bold">
+        <div class="flex items-center">
+          <p>Filter</p>
+          <img src="/images/Path 2.svg" alt="" class="px-3" />
+        </div>
+        <div class="flex items-center bg-[#7C5DFA]  p-1 rounded-full">
+          <img src="/images/+.svg" alt="" class="w-12 h-12 p-4 bg-white rounded-full" />
+          <button class="px-2">New</button>
+        </div>
       </div>
     </div>
     """
